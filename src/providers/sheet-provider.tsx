72dda6ecc89039c45d7c81cb2449604daddd90cc
Brandon Mullins <webdevmullins@gmail.com>
2024-07-06 09:52:36 -0500

    refactor: Fix import statement for NewTransactionSheet in SheetProvider

--- src/providers/sheet-provider.tsx
+++ src/providers/sheet-provider.tsx
@@ -2,7 +2,7 @@
 
 import { NewAccountSheet } from '@/app/(app)/accounts/new-account-sheet'
 import { NewCategorySheet } from '@/app/(app)/categories/new-category-sheet'
-import { NewTransactionSheet } from '@/app/(app)/transactions/new-account-sheet'
+import { NewTransactionSheet } from '@/app/(app)/transactions/new-transaction-sheet'
 import { useMountedState } from 'react-use'
 
 export default function SheetProvider() {
